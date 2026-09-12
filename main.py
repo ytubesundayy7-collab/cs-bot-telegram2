26987b31290e12dac45be2b4e0ddaacc86b27a41
ytubesundayy7-collab <ytubesundayy7@gmail.com>
2026-09-12 18:41:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def setup_handlers(application: Application) -> None:
     application.add_handler(CommandHandler("stats", stats_command))
     application.add_handler(CommandHandler("chatid", chatid_command))
     application.add_handler(CommandHandler("broadcast", broadcast_command))
-
+    application.add_handler(CommandHandler("cancel", cancel_command))
     # Operator replies (only in operator group, must be reply)
     application.add_handler(
         MessageHandler(
